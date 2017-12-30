f7fa88fe904cae513d4b0c6dd53b407f86293d1e
Jay Rambhia <jayrambhia777@gmail.com>
2017-12-30 18:06:19 +0100

    Fixed issue with rotation
    I got confused and projected CropInfo to clockwise rotation instead of anticlockwise rotation. Bitmap has already been rotated clockwise so we need to create a projection for anticlockwise rotation

--- nocropper/src/main/java/com/fenchtose/nocropper/BitmapUtils.java
+++ nocropper/src/main/java/com/fenchtose/nocropper/BitmapUtils.java
@@ -3,7 +3,6 @@ package com.fenchtose.nocropper;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
-import android.util.Log;
 
 /**
  * Created by Jay Rambhia on 11/2/2015.
@@ -16,35 +15,6 @@ public class BitmapUtils {
         }
 
         return BitmapUtils.addPadding(bitmap, cropInfo, cropInfo.paddingColor);
-    }
-
-    public static Bitmap addPadding(Bitmap bmp, int color) throws OutOfMemoryError {
-
-        if (bmp == null) {
-            return null;
-        }
-
-        Bitmap bitmap = null;
-
-        try {
-            int biggerParam = Math.max(bmp.getWidth(), bmp.getHeight());
-            bitmap = Bitmap.createBitmap(biggerParam, biggerParam, bmp.getConfig());
-            Canvas canvas = new Canvas(bitmap);
-            canvas.drawColor(color);
-
-            int top = bmp.getHeight() > bmp.getWidth() ? 0 : (bmp.getWidth() - bmp.getHeight()) / 2;
-            int left = bmp.getWidth() > bmp.getHeight() ? 0 : (bmp.getHeight() - bmp.getWidth()) / 2;
-
-            canvas.drawBitmap(bmp, left, top, null);
-            return bitmap;
-        } catch (OutOfMemoryError e) {
-            if (bitmap != null && !bitmap.isRecycled()) {
-                bitmap.recycle();
-                bitmap = null;
-            }
-
-            throw e;
-        }
     }
 
     public static Bitmap addPadding(Bitmap bmp, CropInfo info, int color) throws OutOfMemoryError {
@@ -64,9 +34,6 @@ public class BitmapUtils {
 
             Rect dest = new Rect(info.horizontalPadding, info.verticalPadding, info.horizontalPadding + info.width, info.verticalPadding + info.height);
             Rect src = new Rect(info.x, info.y, info.x + info.width, info.y + info.height);
-
-            Log.d("BitmapUtils", "src: " + src);
-            Log.d("BitmapUtils", "dest: " + dest);
 
             canvas.drawBitmap(bmp, src, dest, null);
             return bitmap;
